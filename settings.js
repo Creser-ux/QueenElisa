3106d3ff0d49d86bb166bbe3ed33c0e9b273b450
Creser-ux <akinkuotukehinde240@gmail.com>
2024-11-29 00:10:10 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,7 +19,7 @@ module.exports = {
 
 Qᴜᴇᴇɴ ᴇʟɪꜱᴀ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ`,
   ALIVE_MEDIA: 'https://telegra.ph/file/d842677f522b6bef4dfe6.jpg',
-  MONGODB_URL: 'mongodb+srv://Creser:<737247>@creser.u6dl5.mongodb.net/', 
+  MONGODB_URL: 'mongodb+srv://Creser:737247k@creser.9dtc4.mongodb.net/', 
   PREFIX: '@#',
   LANGUAGE: 'EN',
   PORT: 3000,
